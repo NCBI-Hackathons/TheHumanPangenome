c97b13db66cf927978f29adb481f52b8af1bce24
jmonlong <jmonlong@ucsc.edu>
2019-03-27 13:42:40 -0700

    debug

--- SV/HG002/trace.sh
+++ SV/HG002/trace.sh
@@ -2,32 +2,36 @@
 VCF_GRAPH=/data/vg/sv-pop-explicit.vcf.gz
 
 ## SV calls from different dataset
-VCF_SR=/data/vg/HG00514.vcf.gz ## temporary (for testing on a vg call output)
-VCF_SRRD=illumina/vg-call-regional-downsample.vcf.gz
+VCF_SR=/data/vg/HG002-30x-vg.vcf.gz
+VCF_SRRD=/data/vg/HG002-sv-regions-vg.vcf.gz
 VCF_CCS=XX/vg-call.vcf.gz
 VCF_LR=XX/vg-call.vcf.gz
 
 REF=/data/GRCh38/GCA_000001405.15_GRCh38_full_plus_hs38d1_analysis_set.fna
+REFG=/data/GRCh38/hg38.fa
 REGIONS=/data/SVs/SVSummaryPop_EUR-AFR.extract-regions.bed
 
 ## Normalize original VCF
 bcftools norm -m -both -f $REF $VCF_GRAPH | bgzip -c > ${VCF_GRAPH%.vcf.gz}.norm.vcf.gz
 
 ## Short-reads
-bcftools norm -m -both -f $REF $VCF_SR | bgzip -c > ${VCF_SR%.vcf.gz}.norm.vcf.gz
-Rscript callVariantsInInsertedSeq.R ${VCF_GRAPH%.vcf.gz}.norm.vcf.gz ${VCF_SR%.vcf.gz}.norm.vcf.gz variantsInInsertions.SR.tsv $REGIONS
+bcftools norm -m -both -f $REFG $VCF_SR | bgzip -c > ${VCF_SR%.vcf.gz}.norm.vcf.gz
+zcat ${VCF_SR%.vcf.gz}.norm.vcf.gz | awk '{if(substr($0, 1, 1)=="#" || (length($4)==1 && length($5)>20)){print$0}}' | bgzip -c > ${VCF_SR%.vcf.gz}.norm.nosnp.vcf.gz
+Rscript callVariantsInInsertedSeq.R ${VCF_GRAPH%.vcf.gz}.norm.vcf.gz ${VCF_SR%.vcf.gz}.norm.nosnp.vcf.gz /data/variants_in_insertions/HG002/variantsInInsertions.SR.norm $REGIONS
+Rscript filterInternalVariants.R /data/variants_in_insertions/HG002/variantsInInsertions.SR.norm.tsv /data/variants_in_insertions/HG002/variantsInInsertions.SR.norm.filtered.tsv
 
 ## Short-reads regional downsample
-bcftools norm -m -both -f $REF $VCF_SRRD | bgzip -c > ${VCF_SRRD%.vcf.gz}.norm.vcf.gz
-Rscript callVariantsInInsertedSeq.R ${VCF_GRAPH%.vcf.gz}.norm.vcf.gz ${VCF_SRRD%.vcf.gz}.norm.vcf.gz variantsInInsertions.SRRD.tsv $REGIONS
+bcftools norm -m -both -f $REFG $VCF_SRRD | bgzip -c > ${VCF_SRRD%.vcf.gz}.norm.vcf.gz
+zcat ${VCF_SRRD%.vcf.gz}.norm.vcf.gz | awk '{if(substr($0, 1, 1)=="#" || (length($4)==1 && length($5)>20)){print$0}}' | bgzip -c > ${VCF_SRRD%.vcf.gz}.norm.ins.vcf.gz
+Rscript callVariantsInInsertedSeq.R ${VCF_GRAPH%.vcf.gz}.norm.vcf.gz ${VCF_SRRD%.vcf.gz}.norm.ins.vcf.gz /data/variants_in_insertions/HG002/variantsInInsertions.SRRD $REGIONS
 
 ## CCS
 bcftools norm -m -both -f $REF $VCF_CCS | bgzip -c > ${VCF_CCS%.vcf.gz}.norm.vcf.gz
-Rscript callVariantsInInsertedSeq.R ${VCF_GRAPH%.vcf.gz}.norm.vcf.gz ${VCF_CCS%.vcf.gz}.norm.vcf.gz variantsInInsertions.CCS.tsv $REGIONS
+Rscript callVariantsInInsertedSeq.R ${VCF_GRAPH%.vcf.gz}.norm.vcf.gz ${VCF_CCS%.vcf.gz}.norm.vcf.gz /data/variants_in_insertions/HG002/variantsInInsertions.CCS $REGIONS
 
 ## Long-reads
 bcftools norm -m -both -f $REF $VCF_LR | bgzip -c > ${VCF_LR%.vcf.gz}.norm.vcf.gz
-Rscript callVariantsInInsertedSeq.R ${VCF_GRAPH%.vcf.gz}.norm.vcf.gz ${VCF_LR%.vcf.gz}.norm.vcf.gz variantsInInsertions.LR.tsv $REGIONS
+Rscript callVariantsInInsertedSeq.R ${VCF_GRAPH%.vcf.gz}.norm.vcf.gz ${VCF_LR%.vcf.gz}.norm.vcf.gz /data/variants_in_insertions/HG002/variantsInInsertions.LR $REGIONS
 
 ## COMPARE variantsInInsertions.*.tsv FILES (Venn diagram, bar plot)
 ## Both (SR vs SRRD) and (SR vs LR vs CCS)
